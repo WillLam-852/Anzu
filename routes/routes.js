a9f27666ac2654f8b5950b2c26a2763c19e8b5c7
Will Lam <wunyinwilliam@gmail.com>
2022-06-10 16:26:13 +0800

    add subpages

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,23 @@
 module.exports = (app) => {
     
     app.get('/', (req, res) => {
-        res.send({ bye: 'buddy' })
+        res.send({ page: 'main' })
+    })
+
+    app.get('/Tea_ceremony', (req, res) => {
+        res.send({ page: 'Tea_ceremony' })
+    })
+
+    app.get('/Ikebana', (req, res) => {
+        res.send({ page: 'Ikebana' })
+    })
+
+    app.get('/Wagashi', (req, res) => {
+        res.send({ page: 'Wagashi' })
+    })
+
+    app.get('/Yukata', (req, res) => {
+        res.send({ page: 'Yukata' })
     })
 
 }

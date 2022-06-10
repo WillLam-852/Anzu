5e3ad3195523b6c387e3cddaefb164398484e3c6
Will Lam <wunyinwilliam@gmail.com>
2022-06-10 16:20:15 +0800

    bye buddy

--- index.js
+++ index.js
@@ -15,65 +15,67 @@ mongoose.connect(keys.mongoURI, {}, err => {
 
 const app = express()
 
-
-// set up EJS (To be deleted)
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-app.set("view engine", "ejs");
-
-// set up multer for storing uploaded files
-var storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'uploads')
-    },
-    filename: (req, file, cb) => {
-        cb(null, file.fieldname + '-' + Date.now())
-    }
-});
-var upload = multer({ storage: storage });
+const model = require('./models/Model')
+require('./routes/routes')(app)
 
 
-// const model = require('./models/Model')
-const imgModel = require('./models/imageModel')
 
-// the GET request handler that provides the HTML UI
-app.get('/', (req, res) => {
-    imgModel.find({}, (err, items) => {
-        if (err) {
-            console.log(err);
-            res.status(500).send('An error occurred', err);
-        }
-        else {
-            res.render('imagesPage', { items: items });
-        }
-    });
-});
+// // set up EJS (To be deleted)
+// app.use(bodyParser.urlencoded({ extended: false }))
+// app.use(bodyParser.json())
+// app.set("view engine", "ejs");
+
+// // set up multer for storing uploaded files
+// var storage = multer.diskStorage({
+//     destination: (req, file, cb) => {
+//         cb(null, 'uploads')
+//     },
+//     filename: (req, file, cb) => {
+//         cb(null, file.fieldname + '-' + Date.now())
+//     }
+// });
+// var upload = multer({ storage: storage });
 
 
-// the POST handler for processing the uploaded file
+// const imgModel = require('./models/imageModel')
+
+// // the GET request handler that provides the HTML UI
+// app.get('/', (req, res) => {
+//     imgModel.find({}, (err, items) => {
+//         if (err) {
+//             console.log(err);
+//             res.status(500).send('An error occurred', err);
+//         }
+//         else {
+//             res.render('imagesPage', { items: items });
+//         }
+//     });
+// });
+
+
+// // the POST handler for processing the uploaded file
   
-app.post('/', upload.single('image'), (req, res, next) => {
-    var obj = {
-        name: req.body.name,
-        desc: req.body.desc,
-        img: {
-            data: fs.readFileSync(path.join(__dirname, '/uploads/', req.file.filename)),
-            contentType: 'image/png'
-        }
-    }
-    imgModel.create(obj, (err, item) => {
-        if (err) {
-            console.log(err);
-        }
-        else {
-            // item.save();
-            res.redirect('/');
-        }
-    });
-});
+// app.post('/', upload.single('image'), (req, res, next) => {
+//     var obj = {
+//         name: req.body.name,
+//         desc: req.body.desc,
+//         img: {
+//             data: fs.readFileSync(path.join(__dirname, '/uploads/', req.file.filename)),
+//             contentType: 'image/png'
+//         }
+//     }
+//     imgModel.create(obj, (err, item) => {
+//         if (err) {
+//             console.log(err);
+//         }
+//         else {
+//             // item.save();
+//             res.redirect('/');
+//         }
+//     });
+// });
 
 
-// require('./routes/routes')(app)
 
 const PORT = process.env.PORT || 5001
 app.listen(PORT, err => {

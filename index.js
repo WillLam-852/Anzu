869ff9cf5d99fa58c3ec6eaa7c2c5c4e35fd09dc
Will Lam <wunyinwilliam@gmail.com>
2022-06-28 11:48:12 +0800

    fix s3 bugs

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ mongoose.connect(keys.mongoURI, {}, err => {
     console.log('connected to MongoDB')
 })
 
+const app = express()
+
+require('./models/Models')
+require('./routes/routes')(app)
+
 if (process.env.NODE_ENV === 'production') {
     const aws = require('aws-sdk')
     aws.config.region = 'ap-northeast-1'
@@ -38,14 +43,7 @@ if (process.env.NODE_ENV === 'production') {
           res.end();
         });
       });
-}
-
-const app = express()
-
-require('./models/Models')
-require('./routes/routes')(app)
-
-if (process.env.NODE_ENV === 'production') {
+    
     // Express will serve up production assets
     // like our main.js file, or main.css file!
     app.use(express.static('client/build'))

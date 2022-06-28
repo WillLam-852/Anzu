650f926b770c373b47afdea32efc6a7c7603e57c
Will Lam <wunyinwilliam@gmail.com>
2022-06-28 23:21:33 +0800

    add dotenv

--- config/keys.js
+++ config/keys.js
@@ -1,9 +1,8 @@
 // keys.js - figure out what set of credentials to return
 
-if (process.env.NODE_ENV === 'production') {
-    // we are in production - return the prod set of keys
-    module.exports = require('./prod')
-} else {
-    // we are in development - return the dev keys!!!
-    module.exports = require('./dev')
+module.exports = {
+    mongoURI: process.env.MONGO_URL,
+    aws_secret_key_id: process.env.AWS_ACCESS_KEY_ID,
+    aws_secret_access_key: process.env.AWS_SECRET_ACCESS_KEY,
+    s3_bucket_name: process.env.S3_BUCKET_NAME
 }

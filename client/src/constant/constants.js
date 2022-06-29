55467b33dbc76a09a71a9a8571b64de45ff233d0
Will Lam <wunyinwilliam@gmail.com>
2022-06-30 02:01:30 +0800

    building

--- client/src/constant/constants.js
+++ client/src/constant/constants.js
@@ -1,4 +1,4 @@
-export const CONTACT_US_TEXT = `1605 Champion Bldg
+export const CONTACT_US_TEXT = `1605 Champion Building
 287-291 Des Voeux Road Central, Sheung Wan
 上環德輔道中287-291長達大廈1605室
 (上環地鐵站 B 出口行2分鐘)
